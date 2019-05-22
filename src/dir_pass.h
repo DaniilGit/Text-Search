089b3d03d90ceb07569b3752ca7bcd6e0365b3a1
DaniilGit <mrkid0@mail.ru>
2019-05-22 19:14:38 +0700

    Add .h file and include guard

---
+++ src/dir_pass.h
@@ -0,0 +1,7 @@
+#ifndef H_DIRPASS
+#define H_DIRPASS
+
+int dir_pass_windows(char* path, char* string);
+int dir_pass_linux(char* path, char* string);
+
+#endif
---
+++ src/file_search.h
@@ -0,0 +1,6 @@
+#ifndef H_FILESEARCH
+#define H_FILESEARCH
+
+void file_search(char* path, char* word);
+
+#endif
