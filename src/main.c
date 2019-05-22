7ead2056b2418d42fdd8e1d2000091e7b5ca71b6
DaniilGit <mrkid0@mail.ru>
2019-05-22 19:11:56 +0700

    Add variables and main input

---
+++ src/main.c
@@ -0,0 +1,50 @@
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+FILE *logfile;
+int filecounter;
+int g_wordcounter;
+
+#include "dir_pass.h"
+#include "print.h"
+#include "file_search.h"
+
+int main(int argc, char **argv) {
+    g_wordcounter = 0;
+    filecounter = 1;
+
+    if (argc < 2)
+        print(4, NULL);
+
+    char *word;
+    char *path;
+    char *logpath;
+    int i = 0;
+    word = (char *)malloc(sizeof(char));
+    path = (char *)malloc(sizeof(char) * 256);
+    logpath = (char *)malloc(sizeof(char) * 256);
+
+    if (word == NULL || path == NULL || logpath == NULL)
+        print(2, NULL);
+
+    printf("Write log file:");
+    scanf("%s", logpath);
+
+    strcpy(path, argv[2]);
+
+    while (path[i] != '\0')
+        i++;
+    if (path[i - 1] == '/')
+        path[i - 1] = '\0';
+
+    strcpy(word, argv[1]);
+
+    if ((logfile = fopen(logpath, "w")) == NULL)
+        print(1, logpath);
+
+    fclose(logfile);
+
+    return 0;
+}
