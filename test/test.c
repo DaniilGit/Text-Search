4c04ff3b14a9df8c61d9c63eb042e2158d2c4279
DaniilGit <mrkid0@mail.ru>
2019-05-29 22:34:31 +0700

    Add 1 version test file_search

--- test/test.c
+++ test/test.c
@@ -1,18 +1,34 @@
 #include "../thirdparty/ctest.h"
-#include "file_search.c"
+#include "file_search.h"
 #include "print.h"
 #include <stdio.h>
 #include <stdlib.h>
 
+FILE* logfile;
+int filecounter;
+int g_wordcounter;
+
 CTEST(file_search, file_search)
 {
+    filecounter = 1;
+    g_wordcounter = 0;
     FILE* test;
+
+    int result = 6;
+    int expected = 0;
+
     test = fopen("test_file.txt", "w");
+    logfile = fopen("result.txt", "w");
 
     fprintf(test, "test\n");
     fprintf(test, "t.e.s.t\n");
     fprintf(test, "one, two, three\n");
     fprintf(test, "test, one, two\n");
 
+    result = file_search("test_file.txt", "test");
+
     fclose(test);
+    fclose(logfile);
+
+    ASSERT_EQUAL(expected, result);
 }
