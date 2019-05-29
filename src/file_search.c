95e3e799785c51c35cf2c751068ac66e9ebf1e90
Пинов Даниил Игоревич <axsell232@gmail.com>
2019-05-29 12:30:42 +0700

    Change clang-format

--- src/file_search.c
+++ src/file_search.c
@@ -3,101 +3,110 @@
 
 #include "print.h"
 
-FILE *logfile;
+FILE* logfile;
 int filecounter;
 int g_wordcounter;
 
-unsigned short compare(int size, char *a, char *b) {
-  for (int i = 0; i < size; i++)
-    if (a[i] != b[i])
-      return 0;
-  return 1;
-}
-
-unsigned char qch(char ch) {
-  if (ch == ' ' && ch == '(' && ch == ')' && ch == '[' && ch == ']' &&
-      ch == '{' && ch == '}' && ch == ',' && ch == '.' && ch == '<' &&
-      ch == '>' && ch == '/' && ch == '|' && ch == '\\' && ch == '!')
+unsigned short compare(int size, char* a, char* b)
+{
+    for (int i = 0; i < size; i++)
+        if (a[i] != b[i])
+            return 0;
     return 1;
-  return 0;
 }
 
-void file_search(char *path, char *word) {
-  FILE *file;
-  if ((file = fopen(path, "r")) == NULL) {
-    print(3, path);
-    return;
-  }
+unsigned char qch(char ch)
+{
+    if (ch == ' ' && ch == '(' && ch == ')' && ch == '[' && ch == ']'
+        && ch == '{' && ch == '}' && ch == ',' && ch == '.' && ch == '<'
+        && ch == '>' && ch == '/' && ch == '|' && ch == '\\' && ch == '!')
+        return 1;
+    return 0;
+}
 
-  fseek(file, 0, SEEK_END);
-  int size = ftell(file);
-
-  int wordsize = 0;
-  while (word[wordsize] != '\0')
-    wordsize++;
-
-  char *buffer = (char *)malloc(sizeof(char) * wordsize);
-  if (buffer == NULL)
-    print(2, NULL);
-  int line = 0;
-  int linepos = 0;
-  int lineend = 0;
-  int pos = 0;
-  int wordcounter = 1;
-
-  unsigned short once = 1;
-  unsigned short eof = 0;
-  while (!eof) {
-    fseek(file, linepos, SEEK_SET);
-    for (int i;;) {
-      i = ftell(file);
-      char ch = '\0';
-      fread(&ch, 1, 1, file);
-      if (ch == '\n') {
-        lineend = i - 1;
-        break;
-      }
-      if (i == size) {
-        lineend = i;
-        eof = 1;
-        break;
-      }
+void file_search(char* path, char* word)
+{
+    FILE* file;
+    if ((file = fopen(path, "r")) == NULL) {
+        print(3, path);
+        return;
     }
 
-    line++;
-    pos = linepos;
+    fseek(file, 0, SEEK_END);
+    int size = ftell(file);
 
-    char *linestr = (char *)malloc(sizeof(char) * (lineend - linepos + 2));
-    if (linestr == NULL)
-      print(2, NULL);
-    fseek(file, linepos, SEEK_SET);
-    fread(linestr, lineend - linepos + 1, 1, file);
-    linestr[lineend - linepos + 1] = '\0';
+    int wordsize = 0;
+    while (word[wordsize] != '\0')
+        wordsize++;
 
-    while (pos <= lineend - wordsize + 1) {
-      fseek(file, pos, SEEK_SET);
-      fread(buffer, wordsize, 1, file);
-      if (compare(wordsize, word, buffer)) {
-        if (once) {
-          once = 0;
-          fprintf(logfile, "%i)The path to the file: %s\nWord:%s\n",
-                  filecounter, path, word);
-          filecounter++;
+    char* buffer = (char*)malloc(sizeof(char) * wordsize);
+    if (buffer == NULL)
+        print(2, NULL);
+    int line = 0;
+    int linepos = 0;
+    int lineend = 0;
+    int pos = 0;
+    int wordcounter = 1;
+
+    unsigned short once = 1;
+    unsigned short eof = 0;
+    while (!eof) {
+        fseek(file, linepos, SEEK_SET);
+        for (int i;;) {
+            i = ftell(file);
+            char ch = '\0';
+            fread(&ch, 1, 1, file);
+            if (ch == '\n') {
+                lineend = i - 1;
+                break;
+            }
+            if (i == size) {
+                lineend = i;
+                eof = 1;
+                break;
+            }
         }
-        fprintf(logfile, "String number(%i): %i\nString: %s\n", wordcounter,
-                line, linestr);
-        pos += wordsize;
-        wordcounter++;
-        g_wordcounter++;
-      }
-      fseek(file, pos, SEEK_SET);
-      fread(buffer, 1, 1, file);
-      while (pos <= lineend - wordsize + 1 && !qch(buffer[0])) {
-        pos++;
-        fread(buffer, 1, 1, file);
-      }
+
+        line++;
+        pos = linepos;
+
+        char* linestr = (char*)malloc(sizeof(char) * (lineend - linepos + 2));
+        if (linestr == NULL)
+            print(2, NULL);
+        fseek(file, linepos, SEEK_SET);
+        fread(linestr, lineend - linepos + 1, 1, file);
+        linestr[lineend - linepos + 1] = '\0';
+
+        while (pos <= lineend - wordsize + 1) {
+            fseek(file, pos, SEEK_SET);
+            fread(buffer, wordsize, 1, file);
+            if (compare(wordsize, word, buffer)) {
+                if (once) {
+                    once = 0;
+                    fprintf(logfile,
+                            "%i)The path to the file: %s\nWord:%s\n",
+                            filecounter,
+                            path,
+                            word);
+                    filecounter++;
+                }
+                fprintf(logfile,
+                        "String number(%i): %i\nString: %s\n",
+                        wordcounter,
+                        line,
+                        linestr);
+                pos += wordsize;
+                wordcounter++;
+                g_wordcounter++;
+            }
+            fseek(file, pos, SEEK_SET);
+            fread(buffer, 1, 1, file);
+            while (pos <= lineend - wordsize + 1 && !qch(buffer[0])) {
+                pos++;
+                fread(buffer, 1, 1, file);
+            }
+        }
+        linepos = lineend + 2;
+        free(linestr);
     }
-    linepos = lineend + 2;
-    free(linestr);
-  }
 }
